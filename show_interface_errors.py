ca66305fb2a27823e181b0fd7cbc4f5eafbfbbbc
jrcristo <jrcristo@gmail.com>
2022-06-30 10:11:12 -0400

    new code for errors, replacing the old one.

--- show_interface_errors.py
+++ show_interface_errors.py
@@ -14,9 +14,6 @@ if __name__ == '__main__':
 
     interface = input("Whats the interface: ")
 
-    yes_option = ['yes', 'y']
-    no_option = ['no', 'n']
-
     # connecting to the device(s)
     JC = funtions_jose.get_credentials_and_interface()
     net_connect = ConnectHandler(**JC)
@@ -24,70 +21,50 @@ if __name__ == '__main__':
 
     # Getting interface Details
     output = net_connect.send_command('show int' + " " + interface + " " + '| inc down|up|drops|CRC|media|collision'
-                                                                       '|resets|MTU|Hardw')
-    # Printing interface_UP
-    if_up = re.search(r'^(Gi|Te|Vl).*', output)
-    if_up_ = re.search(r'^(Gi|Te|Vl)\S+\s\w+\s\w+', if_up.group())
-    line_up = re.search(r'line\sp.*', output)
-    print(if_up_.group() + " " + line_up.group())
+                                                                           '|resets|MTU|Hardw')
+
+    # interface status
+    if 'down' in output:
+        print('===> Interface' + " " + interface + " " + 'is DOWN <===')
+    else:
+        print('===> Interface' + " " + interface + " " + 'is UP <===')
+
+    # getting MTU
+    mtu = re.search(r'MTU\s(\d+)\sby\w+.\s\w+\s(\d+\s\w+.s\w+).\sD\w+\s(\d+\s\w+)', output)
+    print('MTU Value is = ', mtu.group(1))
 
     # Getting Interface mac-add
     mac = re.search(r'([0-9]|[a-z]){4}\.\S+', output)
-    print("The hardcoded interface mac-add is: ", mac.group())
+    print("The hardcoded interface mac-add is:", mac.group())
+    print('Bandwidth is =', mtu.group(2))
+    print('Delays is =', mtu.group(3))
 
-    if "duplex" in output:
+    if 'Full-duplex' in output:
         # Speed Interface
-        speed = re.search(r'(Full|Half)-\w+,\s\d+\w+/s', output)
-        speed_filter = re.search(r'\d+.*', speed.group())
-        print('The speed is: ', speed_filter.group())
+        speed = re.search(r'Full-\w+.\s(\d+\w+.\w+)', output)
+        print('The speed is:', speed.group(1))
 
-        # Printing Duplex
-        duplex = re.search(r'(Full|Half)-d\w+', output)
-        print('The duplex is: ', duplex.group())
-
-        # Collisions
-        in_collisions = re.search(r'\d+\sco\w+', output)
-        collisions_total = re.search(r'\d', in_collisions.group())
-        print("Total of input collisions errors are: ", collisions_total.group())
+        # getting duplex
+        duplex = re.search(r'.*\bduplex\b', output)
+        print('The duplex is:', duplex.group())
 
     else:
-        print('==> Probably this is a SVI interface <==')
+        pass
+
+    # output errors, Collisions and reset
+    collisions = re.search(r'(\d+)\sout\w+\ser\w+,\s(\d+)\sco\w+.\s(\d+)', output)
+    print('Total output error =', collisions.group(1))
+    print("Total of output collisions errors =", collisions.group(2))
+    print('Total output interface reset =', collisions.group(3))
 
     # Printing Drops
-    drops = re.search(r'drops:\s.*', output)
-    drops_total = re.search(r'\d+', drops.group())
-    print("Total Drops on interface are: ", drops_total.group())
+    drops = re.search(r'\boutput\sdrops.\s(\d+)\b', output)
+    print("Total Drops on interface =", drops.group(1))
 
-    # input_errors
-    in_error = re.search(r'\d\sinput\s\w+', output)
-    in_total_err = re.search(r'\d+', in_error.group())
-    print("Total Input errors on Interface are: ", in_total_err.group())
-
-    # CRC error
-    in_crc = re.search(r'\d+\sCRC', output)
-    crc_total = re.search(r'\d', in_crc.group())
-    print("Total of input CRC errors are: ", crc_total.group())
-
-    # interface rests
-    int_reset = re.search(r'\d+\sinter\w+.*', output)
-    reset_total = re.search(r'\d+', int_reset.group())
-    print("Total of input interface reset errors are: ", reset_total.group())
-
-    # Getting MTU interface value
-    mtu = re.search(r'MTU\s\d+\s\w+', output)
-    mtu_total = re.search(r'\d+\s\w+', mtu.group())
-    print("MTU value is: ", mtu_total.group())
-
-    # Getting BW
-    bw = re.search(r'BW\s\d+\s\w+', output)
-    bw_filter = re.search(r'\d+\s\w+', bw.group())
-    print("The interface bandwidth is: ", bw_filter.group())
-
-    # Getting Load
-    load = re.search(r'DLY\s\d+\s\w+', output)
-    load_filter = re.search(r'\d+\s\w+', load.group())
-    print("The interface load is: ", load_filter.group())
-
+    # Input Errors and CRC
+    input_errors = re.search(r'(\d+)\sinput\serr\w+.\s(\d+)', output)
+    print('Total Input Error =', input_errors.group(1))
+    print('Total Input CRC errors =', input_errors.group(2))
 
 
 
