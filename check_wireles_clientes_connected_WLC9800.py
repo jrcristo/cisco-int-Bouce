83025d77dcde14a05737f930fd1f91adbf00f737
jrcristo <jrcristo@gmail.com>
2022-08-24 10:01:21 -0400

    added XIC

--- check_wireles_clientes_connected_WLC9800.py
+++ check_wireles_clientes_connected_WLC9800.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     try:
         ex = re.match('^EX', ap_name)
         xp = re.match('^XP', ap_name)
+        xic = re.match('^XIC', ap_name)
 
     except AttributeError:
         pass
@@ -46,4 +47,18 @@ if __name__ == '__main__':
     except AttributeError:
         pass
 
+    try:
+        if xic.group():
+            isIP = '10.126.140.125'
+            print('==> Connecting to XIC WLC at' + " " + isIP)
+            JC = funtions_jose.connect_wlc(isIP)
+            net_connect = ConnectHandler(**JC)
+            net_connect.enable()
+
+            # getting data
+            output = funtions_jose.get_wlc_9800_clients_connected_by_ap(ap_name, net_connect)
+            exit(0)
+
+    except AttributeError:
+        pass
 
