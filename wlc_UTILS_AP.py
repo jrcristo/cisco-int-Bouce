1d57f9d1adaff08223beefbfd334400bf804c42c
jrcristo <jrcristo@gmail.com>
2022-10-04 23:53:47 -0400

    Added IOS WLC(9800)

--- wlc_UTILS_AP.py
+++ wlc_UTILS_AP.py
@@ -10,6 +10,9 @@ if __name__ == '__main__':
     ap_name = input("What's the AP name: ")
 
     try:
+        xic = re.match('^XIC', ap_name)
+        xp = re.match('^XP', ap_name)
+        ex = re.match('^EX', ap_name)
         yp = re.match('^YP', ap_name)
         gp = re.match('^GP', ap_name)
         rp = re.match('^RP', ap_name)
@@ -24,6 +27,51 @@ if __name__ == '__main__':
         ru = re.match('^RU', ap_name)
         sa = re.match('^SA', ap_name)
         pev2 = re.match('^PEV', ap_name)
+
+    except AttributeError:
+        pass
+
+    try:
+        if xic.group():
+            isIP = '10.126.140.125'
+            print('==> Connecting to XIC WLC at' + " " + isIP)
+            JC = funtions_jose.connect_wlc(isIP)
+            net_connect = ConnectHandler(**JC)
+            net_connect.enable()
+
+            # connecting WLC
+            output = funtions_jose.wlc_reboot_aireos_ap(ap_name, net_connect)
+            exit(0)
+
+    except AttributeError:
+        pass
+
+    try:
+        if xp.group():
+            isIP = '10.125.135.225'
+            print('==> Connecting to XP=Discovery WLC at' + " " + isIP)
+            JC = funtions_jose.connect_wlc(isIP)
+            net_connect = ConnectHandler(**JC)
+            net_connect.enable()
+
+            # connecting WLC
+            output = funtions_jose.wlc_reboot_aireos_ap(ap_name, net_connect)
+            exit(0)
+
+    except AttributeError:
+        pass
+
+    try:
+        if ex.group():
+            isIP = '10.125.71.225'
+            print('==> Connecting to EX=Enchanted WLC at' + " " + isIP)
+            JC = funtions_jose.connect_wlc(isIP)
+            net_connect = ConnectHandler(**JC)
+            net_connect.enable()
+
+            # connecting WLC
+            output = funtions_jose.wlc_reboot_aireos_ap(ap_name, net_connect)
+            exit(0)
 
     except AttributeError:
         pass
