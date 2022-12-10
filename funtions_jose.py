3cb3db8f8263e91e703ca4f12f88bd033d7a3410
Jose Cristo <jrcristo@gmail.com>
2022-12-10 11:41:56 -0500

    added validation to wlc_utils_ap function if you provide a mac-add

--- funtions_jose.py
+++ funtions_jose.py
@@ -1945,6 +1945,11 @@ def wlc_utils_ap(ap_name, net_connect):
 
     if 'Incorrect usage' in version:
         print('==> Using aireOS WLC <==')
+        # validation if you enter a mac-add
+        if re.match(r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})', ap_name):
+            if_mac_add = net_connect.send_command('show ap config general ' + ap_name, read_timeout=801)
+            ap_name = re.search(r'Cisco\sAP\sNa\S+\s(.*)', if_mac_add).group(1)
+
         # checking AP neighbor
         cdp = net_connect.send_command('show ap cdp neighbors ap-name ' + ap_name)
         inv = net_connect.send_command('show ap inventory ' + ap_name)
